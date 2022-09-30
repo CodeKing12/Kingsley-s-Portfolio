69fee6260430c65c3453faa27388cb92fc6a573b
Eyetu Kingsley <eyetukingsley330@gmail.com>
2022-09-30 23:06:37 +0100

    I have added the write-up for the about me section. With that, I am now officially done with my portfolio website (save the backend portion).

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -3,13 +3,14 @@
         <h4 class="font-mono font-bold text-lg sm2:text-xl uppercase w-fit mx-auto cursor-pointer after:block after:w-2/3 lg:after:w-2/6 after:delay-100 after:mx-0 lg:after:mx-auto after:mt-1 after:bg-neon-green after:h-0.5 hover:after:w-full hover:after:opacity-100 after:duration-500 after:ease" data-aos="zoom-out" data-aos-easing="ease-in-sine" data-aos-duration="600" data-aos-offset="120">About Me</h4>
         <div class="flex flex-col mt-8 sm:mt-10 lg:mt-14 font-metrophobic text-lg leading-loose gap-10">
             <p data-aos="fade-up" data-aos-easing="ease" data-aos-duration="700" data-aos-offset="50">
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                I'm Eyetu Kingsley, a fullstack developer with an array of expertise in frontend and backend languages, frameworks and tools. I also possess extensive experience in converting Figma designs to responsive and interactive web pages, with valuable practical knowledge in editing templates.
+                <!-- Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. -->
             </p>
             <p data-aos="fade-left" data-aos-easing="ease" data-aos-duration="700" data-aos-offset="50">
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-            </p>    
+                I started my software development journey pver 2 years ago with Python. Since then, I've learnt new tools and languages to get the job done while delivering the very best I can. To give back to the community, I also develop talents by training students on frontend development in my free time.
+            </p>
             <p data-aos="fade-right" data-aos-easing="ease" data-aos-duration="700" data-aos-offset="50">
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                I have worked as a freelancer and as a full-time employee with TraterDev and PythonBay as a Frontend Developer and Pythonista. For my projects, I primarily use Django & MySQL for the backend, with Vue and TailwindCSS for the frontend. I also create most my own designs using Figma or on-the-fly when the need arises, and sometimes acquire and edit templates if deemed necessary.
             </p>
         </div>
     </section>
