b51b1b8f2dcb1bb93f3271660bfbeee1b7ffbdc0
Eyetu Kingsley <eyetukingsley330@gmail.com>
2022-11-16 13:34:53 +0100

    I added a url for shoppa

--- src/components/BackendProjects.vue
+++ src/components/BackendProjects.vue
@@ -11,7 +11,7 @@
                     <h3 class="uppercase font-mono text-lg font-bold">Shoppa</h3>
                     <p class="font-capriola text-[15px] leading-loose">Shoppa is an ecommerce platform that specializes in the sales of laptops, phones and games across Nigeria. The Frontend and Backend were created by me using HTML, CSS, JS, Django amongst other libraries.</p>
                     <button class="font-mono font-bold border-2 border-neon-green text-center px-9 py-2 relative after:h-full after:w-0 after:bg-neon-green after:block after:absolute after:top-0 after:left-0 after:z-10 hover:after:w-full hover:text-black after:duration-300 after:ease-in-out w-fit">
-                        <span class="relative z-20 delay-100 duration-[400ms] font-grotesk">View Project</span>
+                        <a class="relative z-20 delay-100 duration-[400ms] font-grotesk" href="https://shoppa-ng.herokuapp.com/">View Project</a>
                     </button>
                 </div>
             </div>
